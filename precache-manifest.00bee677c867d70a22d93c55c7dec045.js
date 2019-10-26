e2940839454be3675d9997a7733a4449202740b6
Sharon Choong <53707263+sharonchoong@users.noreply.github.com>
2019-10-26 00:05:36 -0400

    Updates

---
+++ precache-manifest.00bee677c867d70a22d93c55c7dec045.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e9239c8a4ae9daff499ac92c7849d7d",
+    "url": "/nightstand/index.html"
+  },
+  {
+    "revision": "13bf473f7f679f16aae7",
+    "url": "/nightstand/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "9b1ee8221cbdff1d712b",
+    "url": "/nightstand/static/css/main.23de05fa.chunk.css"
+  },
+  {
+    "revision": "13bf473f7f679f16aae7",
+    "url": "/nightstand/static/js/2.fbf26894.chunk.js"
+  },
+  {
+    "revision": "9b1ee8221cbdff1d712b",
+    "url": "/nightstand/static/js/main.e3c8f8c6.chunk.js"
+  },
+  {
+    "revision": "7938e58079cd72da797d",
+    "url": "/nightstand/static/js/runtime-main.58bdee8c.js"
+  },
+  {
+    "revision": "1e7b161afdf2971a56022fdef5856c2f",
+    "url": "/nightstand/static/media/settings_applications.1e7b161a.svg"
+  }
+]);
